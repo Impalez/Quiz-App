7bbd2104e98e6ec9c92bee6448ea0cd5986f39ce
Alexandru Radulescu <alex.radulescu88@yahoo.com>
2020-08-26 00:02:15 +0300

    Improve app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">Quiz App
-    </div>
-  );
+  return <div className="App">Quiz Application</div>;
 }
 
 export default App;
